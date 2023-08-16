3e85e66cd0139e02bbdeb701314dd14107f3479a
Ian Bruce <ianbruce.braincue@gmail.com>
2023-08-16 20:15:27 +0100

    fix: navigation component bug

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,4 +1,4 @@
-import Navigation from './components/Navigation';
+import Navigation from './components/navigation';
 
 import './styles/error-page.scss';
 
--- src/ProjectLayout.js
+++ src/ProjectLayout.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link, Outlet  } from 'react-router-dom';
-import Navigation from './components/Navigation';
+import Navigation from './components/navigation';
 import { motion, AnimatePresence } from "framer-motion";
 import { ReactComponent as ArrowRight } from './assets/icon-arrow-right.svg';
 import { ReactComponent as ChevronRight } from './assets/icon-chevron-right.svg';
