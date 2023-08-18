847fa8f5e0abb933d036597d651e60a4ca8f1e6f
Ian Bruce <ianbruce.braincue@gmail.com>
2023-08-18 18:03:44 +0100

    update: noise effect to separate component, wip: nav mix belnd mode when active/open

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import Noise from './Noise';
 import Home from './pages/Home';
 import CurriculumVitae from './pages/CurriculumVitae';
 import ProjectLayout from './ProjectLayout';
@@ -6,14 +7,19 @@ import Project from './pages/Project';
 import ErrorPage from './ErrorPage';
 
 function App() {
-    return <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cv" element={<CurriculumVitae />} />
-        <Route path="/projects" element={<ProjectLayout />}>
-            <Route path=":id" element={<Project />}/>
-        </Route>
-        <Route path="*" element={<ErrorPage />} />
-    </Routes>
+    return (
+        <>
+        <Noise />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cv" element={<CurriculumVitae />} />
+            <Route path="/projects" element={<ProjectLayout />}>
+                <Route path=":id" element={<Project />}/>
+            </Route>
+            <Route path="*" element={<ErrorPage />} />
+        </Routes>
+        </> 
+    )
 }
 
 export default App
---
+++ src/Noise.js
@@ -0,0 +1,7 @@
+export default function Noise() {
+    return (
+        <div className="noise-wrapper">
+            <div className="noise"></div>
+        </div>
+    );
+}
